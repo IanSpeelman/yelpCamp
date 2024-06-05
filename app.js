af266b8a53449d56338d563626c2eb6dde9a089e
Ian Speelman <ianspeelman@gmail.com>
2024-06-05 15:28:29 +0200

    add create campground functionality

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const Campgrounds = require("./models/campgrounds")
+const express = require("express");
+const mongoose = require("mongoose");
+const Campgrounds = require("./models/campgrounds");
 
-const app = express()
+const app = express();
 const port = 3001;
-mongoose.connect("mongodb://localhost:27017/yelp-camp")
-.then(() => console.log("database connected"))
-.catch(err => console.log(err))
-app.set("views", `${__dirname}/views`)
-app.set("view engine", "ejs")
+mongoose
+	.connect("mongodb://localhost:27017/yelp-camp")
+	.then(() => console.log("database connected"))
+	.catch((err) => console.log(err));
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "ejs");
 
-app.get("/", (req,res) => {
-      res.render("home")
-})
-app.get("/campgrounds", async (req,res) => {
-      const campgrounds = await Campgrounds.find({})
-      res.render("campgrounds/index", { campgrounds })
-})
-app.get("/campgrounds/:id", async (req,res) => {
-      const {id} = req.params
-      const campground = await Campgrounds.findById(id)
-      res.render("campgrounds/show", {campground})
-})
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, () => console.log("server started"))
+app.get("/", (req, res) => {
+	res.render("home");
+});
+app.get("/campgrounds", async (req, res) => {
+	const campgrounds = await Campgrounds.find({});
+	res.render("campgrounds/index", { campgrounds });
+});
+app.get("/campgrounds/new", (req, res) => {
+	res.render("campgrounds/new");
+});
+app.post("/campgrounds", async (req, res) => {
+	const { title, location } = req.body.campground;
+	const newCampground = new Campgrounds({ title, location });
+	newCampground.save();
+      res.redirect(`/campgrounds/${newCampground._id}`)
+});
+app.get("/campgrounds/:id", async (req, res) => {
+	const { id } = req.params;
+	const campground = await Campgrounds.findById(id);
+	res.render("campgrounds/show", { campground });
+});
+
+app.listen(port, () => console.log("server started"));
